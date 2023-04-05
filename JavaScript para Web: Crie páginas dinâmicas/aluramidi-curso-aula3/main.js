a75b11354dbad3ee8d930ef461a31b644107ff3d
Renan Amorim <renanamorim89@hotmail.com>
2023-04-05 05:53:50 -0300

    for

--- JavaScript para Web: Crie páginas dinâmicas/aluramidi-curso-aula3/main.js
+++ JavaScript para Web: Crie páginas dinâmicas/aluramidi-curso-aula3/main.js
@@ -6,22 +6,32 @@ const keyList = document.querySelectorAll('.tecla');
 
 keyList[0].onclick = tocaSom;
 
-let contador = 0;
+// let contador = 0;
 
-while (contador < keyList.length) {
+// while (contador < keyList.length) {
+//   const key = keyList[contador];
+
+//   const instrumento = key.classList[1];
+
+//   //template string
+//   const idAudio = `#som_${instrumento}`;
+//   console.log(idAudio);
+
+//   keyList[contador].onclick = function () {
+//     tocaSom(idAudio);
+//   };
+
+//   contador = contador + 1;
+
+//   console.log(contador);
+// }
+
+for (let contador = 0; contador < keyList.length; contador++) {
   const key = keyList[contador];
-
   const instrumento = key.classList[1];
-
-  //template string
   const idAudio = `#som_${instrumento}`;
-  console.log(idAudio);
 
   keyList[contador].onclick = function () {
     tocaSom(idAudio);
   };
-
-  contador = contador + 1;
-
-  console.log(contador);
 }
--- JavaScript para Web: Crie páginas dinâmicas/exercicio/index.js
+++ JavaScript para Web: Crie páginas dinâmicas/exercicio/index.js
@@ -1,1 +1,10 @@
-document.querySelector('input[type=tel]');
+const listaDeTeclas = document.querySelectorAll('input[type=button]');
+const inputTel = document.querySelector('input[type=tel]');
+
+for (i = 0; i < listaDeTeclas.length; i++) {
+  const tecla = listaDeTeclas[i];
+
+  tecla.onclick = function () {
+    inputTel.value = inputTel.value + tecla.value;
+  };
+}
