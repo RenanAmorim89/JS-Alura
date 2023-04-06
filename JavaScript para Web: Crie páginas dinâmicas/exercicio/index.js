89fbb56e8cb7a9234d0e9fdf97a16b7f2a8dda7a
Renan Amorim <renanamorim89@hotmail.com>
2023-04-06 08:50:58 -0300

    terminio do alura midi

--- JavaScript para Web: Crie páginas dinâmicas/exercicio/index.js
+++ JavaScript para Web: Crie páginas dinâmicas/exercicio/index.js
@@ -7,4 +7,12 @@ for (i = 0; i < listaDeTeclas.length; i++) {
   tecla.onclick = function () {
     inputTel.value = inputTel.value + tecla.value;
   };
+
+  tecla.onkeydow = function (evento) {
+    if (evento.code === 'Enter' || evento.code === 'Space')
+      tecla.classList.add('ativa');
+  };
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  };
 }
