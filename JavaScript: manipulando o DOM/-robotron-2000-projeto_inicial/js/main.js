c59e3fce1fae34c760c89d5eb42b7eda599f7e77
Renan Amorim <renanamorim89@hotmail.com>
2023-04-07 10:05:07 -0300

    Manipulando o Dom

---
+++ JavaScript: manipulando o DOM/-robotron-2000-projeto_inicial/js/main.js
@@ -0,0 +1,11 @@
+const robotron = document.querySelector('#robotron');
+
+robotron.addEventListener('click', (evento) => {
+  console.log(evento);
+});
+
+function dizOi(nome) {
+  console.log(`oi ${nome}`);
+  console.log('Bem-Vindo ao robotron 2000');
+}
+dizOi('Renan');
